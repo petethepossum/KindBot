1666247d4848e56cc30048c271daf39865a1030b
petethepossum <venemouspossum@gmail.com>
2024-03-18 11:03:25 +1300

    This is slighlty broken on my laptop but it works

--- Client/client.py
+++ Client/client.py
@@ -1,6 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-flask_server_url = 'http://http://192.168.68.116:5001' 
+flask_server_url = 'http://172.20.10.8:5001'
 
+# Send request to Flask server to get input text
 response = requests.get(flask_server_url)
-print(response)
+
+if response.status_code == 200:
+    # Parse HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # Extract text content
+    text_content = soup.get_text()
+    print("Received text from server:")
+    print(text_content)
+else:
+    print("Failed to get text from server")
--- Server /app.py
+++ Server /app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/parse', methods=['POST'])
+def parse_text():
+    text = request.form['text']
+    return text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-   
